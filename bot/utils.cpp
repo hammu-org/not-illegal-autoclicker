9efe0e8c38cd5244845d825ce90636c8a87e0fce
tinni <55824963+tibytinneh@users.noreply.github.com>
2025-07-12 05:49:21 +0800

    add: randomOffset fn and getCurrentMousePos

--- bot/utils.cpp
+++ bot/utils.cpp
@@ -1,17 +1,55 @@
 #include "utils.hpp"
 
+#ifdef _WIN32
+#include <windows.h>
+#elif defined(__APPLE__)
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+Point getCurrentMousePos()
+{
+#ifdef _WIN32
+    POINT p;
+    if (GetCursorPos(&p))
+    {
+        return {static_cast<int>(p.x), static_cast<int>(p.y)};
+    }
+    else
+    {
+        return {0, 0};
+    }
+
+#elif defined(__APPLE__)
+    CGEventRef event = CGEventCreate(NULL);
+    CGPoint loc = CGEventGetLocation(event);
+    CFRelease(event);
+    return {static_cast<int>(loc.x), static_cast<int>(loc.y)};
+#else
+    return {0, 0};
+#endif
+}
+
 std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 
-int getRandomInt(int min, int max) {
+int getRandomInt(int min, int max)
+{
     std::uniform_int_distribution<int> dist(min, max);
     return dist(rng);
 }
 
-double getRandomDouble(double min, double max) {
+double getRandomDouble(double min, double max)
+{
     std::uniform_real_distribution<double> dist(min, max);
     return dist(rng);
 }
 
-void sleepRandom(int minMs, int maxMs) {
+void sleepRandom(int minMs, int maxMs)
+{
     std::this_thread::sleep_for(std::chrono::milliseconds(getRandomInt(minMs, maxMs)));
+}
+
+int randomOffset(int base, int range)
+{
+    std::uniform_int_distribution<int> dist(-range, range);
+    return base + dist(rng);
 }
--- bot/utils.hpp
+++ bot/utils.hpp
@@ -2,7 +2,19 @@
 #include <chrono>
 #include <random>
 #include <thread>
+#include <iostream>
+
+struct Point
+{
+    int x;
+    int y;
+
+    bool isValid() const { return x >= 0 && y >= 0; }
+};
+
+Point getCurrentMousePos();
 
 int getRandomInt(int min, int max);
 double getRandomDouble(double min, double max);
 void sleepRandom(int minMs, int maxMs);
+int randomOffset(int base, int range);
