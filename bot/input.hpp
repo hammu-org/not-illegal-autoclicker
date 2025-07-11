d63c1995e0ba545ca5227039e671666c063fd42c
Roby Tanama <54106843+tanamaroby@users.noreply.github.com>
2025-07-11 17:08:08 +0800

    Remove mouse input functions and related header; clean up main.cpp formatting

--- bot/input.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void moveMouse(int x, int y);
-void clickMouse();
--- bot/main.cpp
+++ bot/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 #include "WindMouse.hpp"
-#include "input.hpp"
 #include "utils.hpp"
 
-int main() {
-    std::cout << "Program start." << std::endl;
+int main()
+{
+  std::cout << "Program start." << std::endl;
 
-    int startX = 500, startY = 500;
-    int endX = getRandomInt(100, 800);
-    int endY = getRandomInt(100, 600);
+  int startX = 500, startY = 500;
+  int endX = getRandomInt(100, 800);
+  int endY = getRandomInt(100, 600);
 
-    windMouse(startX, startY, endX, endY);
+  windMouse(startX, startY, endX, endY);
 
-    std::cout << "Program end." << std::endl;
+  std::cout << "Program end." << std::endl;
 }
